73d09fb752b887be944302ce25696fea936e6424
Bishnu Nath <sintodas1996@gmail.com>
2023-11-09 16:23:32 +0600

    website name By Helmet

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,10 +3,17 @@ import Services from "../Services/Services";
 import FAQSection from "../../components/FAQSection/FAQSection";
 import AboutUs from "../AboutUs/AboutUs";
 import LatestNews from "../../components/LatestNews/LatestNews";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <div className="py-5">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Local Tours and Guide</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
       <Banner></Banner>
 
       <Services></Services>
